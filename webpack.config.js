27e3b0fe07e111232042aaf39ef8625eb50dacdf
Shane Tenorio <shane.tenorio@cerner.com>
2019-10-16 15:41:12 -0500

    Add React component

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: ['@babel/polyfill', './lib/components/index.js'],
+  output: {
+      path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/,
+        use: 'babel-loader' 
+    }
+    ]
+  }
+};
